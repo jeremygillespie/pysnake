6648dee375cdc850d7b43f91ade89366ff3748a0
Jeremy Gillespie <jeremy@gillespiegames.com>
2021-01-03 10:11:26 -0800

    add directions

--- graph.py
+++ graph.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+north = (0, 1)
+south = (0, -1)
+east = (1, 0)
+west = (-1, 0)
+
+
+def offset(a, b):
+    return tuple(np.array(a) + np.array(b))
+
 
 class Graph:
     def __init__(self, w, h):
