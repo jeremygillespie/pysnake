5c49240eb7cf1ba925976a00e2bca5d6c68c0da8
Jeremy Gillespie <jeremy@gillespiegames.com>
2021-01-08 18:22:59 -0800

    kinda oop now

--- engine.py
+++ engine.py
@@ -1,6 +1,5 @@
 import snake
 import numpy as np
 
-occupied = np.full((10, 10), 0, dtype=int)
-
-print(snake.output(occupied))
+g = snake.occupied()
+print(g)
--- snake.py
+++ snake.py
@@ -20,77 +20,67 @@ west = point(-1, 0)
 cardinals = [north, south, east, west]
 
 
-class graph(np.ndarray):
-    def __new__(subtype, shape, dtype=float, buffer=None, offset=0,
-                strides=None, order=None):
-        obj = super(InfoArray, subtype).__new__(subtype, shape, dtype,
-                                                buffer, offset, strides,
-                                                order)
+class graph:
+    def __init__(self, width, height, fill_value=0, dtype=bool):
+        self.val = np.full((width, height), fill_value, dtype=dtype)
 
-        # obj.info = info
+    width = property(lambda self: self.val.shape[0])
+    height = property(lambda self: self.val.shape[1])
 
-        return obj
+    def __call__(self):
+        return self.val
 
-    def __array_finalize__(self, obj):
-        if obj is None:
-            return
+    def inbounds(self, p):
+        if p.x < 0 or p.y < 0:
+            return False
 
-        # self.info = getattr(obj, 'info', None)
+        if p.x >= self.width or p.y >= self.height:
+            return False
 
-
-def width(graph):
-    return graph.shape[0]
+        return True
 
 
-def height(graph):
-    return graph.shape[1]
+class occupied(graph):
+    def __init__(self, width, height):
+        graph.__init__(self, width, height, dtype=int)
 
+    def movable(self, p):
+        return self()[p] > 1
 
-def inbounds(graph, p):
-    if p.x < 0 or p.y < 0:
-        return False
+    def incoming(self, p):
+        for direc in cardinals:
+            p1 = p + direc
+            if self.inbounds(p1) and self()[p1] > 0 and self()[p1] == self()[p] - 1:
+                return direc * -1
 
-    if p.x >= width(graph) or p.y >= height(graph):
-        return False
+    def __str__(self):
+        result = ''
+        for y in range(self.height - 1, -1, -1):
+            for x in range(self.width):
+                p = point(x, y)
+                if self.incoming(p) == south:
+                    result += '∨   '
+                elif self.inbounds(p + north) and self.incoming(p + north) == north:
+                    result += '∧   '
+                else:
+                    result += '    '
 
-    return True
+            result += '\n'
 
+            for x in range(self.width):
+                p = point(x, y)
+                if self()[p]:
+                    result += '# '
+                else:
+                    result += '. '
 
-def incoming(occ, p):
-    for direc in cardinals:
-        p1 = p + direc
-        if inbounds(occ, p1) and occ[p1] > 0 and occ[p1] == occ[p] - 1:
-            return direc * -1
+                if self.incoming(p) == west:
+                    result += '< '
+                elif self.inbounds(p + east) and self.incoming(p + east) == east:
+                    result += '> '
+                else:
+                    result += '  '
 
+            result += '\n'
 
-def output(occ):
-    result = ''
-    for y in range(height(occ) - 1, -1, -1):
-        for x in range(width(occ)):
-            p = point(x, y)
-            if incoming(occ, p) == south:
-                result += '∨   '
-            elif inbounds(occ, p + north) and incoming(occ, p + north) == north:
-                result += '∧   '
-            else:
-                result += '    '
-
-        result += '\n'
-
-        for x in range(width(occ)):
-            p = point(x, y)
-            if occ[p]:
-                result += '# '
-            else:
-                result += '. '
-
-            if incoming(occ, p) == west:
-                result += '< '
-            elif inbounds(occ, p + east) and incoming(occ, p + east) == east:
-                result += '> '
-            else:
-                result += '  '
-
-        result += '\n'
-
-    return result
+        return result
