27aa54b5a1a4333f961eeaf8fed82ca52393d856
Jeremy Gillespie <jeremy@gillespiegames.com>
2021-01-15 18:11:02 -0800

    add corner index

--- engine.py
+++ engine.py
@@ -1,5 +1,9 @@
 import snake
 import numpy as np
 
-g = snake.occupied()
+g = snake.occupied_corners()
 print(g)
+print(g.cor)
+print(g.cor_map)
+print(g.cor[g.cor_map])
+print(g.cor[g.cor_map[0, 1]])
--- snake.py
+++ snake.py
@@ -20,15 +20,22 @@ west = point(-1, 0)
 cardinals = [north, south, east, west]
 
 
-class graph:
-    def __init__(self, width, height, fill_value=0, dtype=bool):
-        self.val = np.full((width, height), fill_value, dtype=dtype)
+class occupied:
+    def __init__(self, copy=None, width=10, height=10):
+        if copy == None:
+            self.occ = np.full((width, height), 0, dtype=int)
+        else:
+            self.occ = np.copy(copy.grid)
 
-    width = property(lambda self: self.val.shape[0])
-    height = property(lambda self: self.val.shape[1])
+    width = property(lambda self: self.occ.shape[0])
+    height = property(lambda self: self.occ.shape[1])
 
-    def __call__(self):
-        return self.val
+    def step(self, p, length):
+        self.occ[self.occ > 0] -= 1
+        self.occ[p] = length
+
+    def eat(self, p, new_length):
+        self.occ[p] = new_length
 
     def inbounds(self, p):
         if p.x < 0 or p.y < 0:
@@ -39,19 +46,20 @@ class graph:
 
         return True
 
-
-class occupied(graph):
-    def __init__(self, width, height):
-        graph.__init__(self, width, height, dtype=int)
-
     def movable(self, p):
-        return self()[p] > 1
+        return self.inbounds(p) and self.occ[p] > 1
 
     def incoming(self, p):
         for direc in cardinals:
             p1 = p + direc
-            if self.inbounds(p1) and self()[p1] > 0 and self()[p1] == self()[p] - 1:
+            if self.inbounds(p1) and self.occ[p1] > 0 and self.occ[p1] == self.occ[p] - 1:
                 return direc * -1
+
+    def outgoing(self, p):
+        for direc in cardinals:
+            p1 = p + direc
+            if self.inbounds(p1) and self.occ[p1] == self.occ[p] + 1:
+                return direc
 
     def __str__(self):
         result = ''
@@ -69,7 +77,7 @@ class occupied(graph):
 
             for x in range(self.width):
                 p = point(x, y)
-                if self()[p]:
+                if self.occ[p]:
                     result += '# '
                 else:
                     result += '. '
@@ -84,3 +92,43 @@ class occupied(graph):
             result += '\n'
 
         return result
+
+
+class index(namedtuple('index', 'xx yy')):
+    def __getitem__(self, key):
+        return self.xx[key], self.yy[key]
+
+    def __setitem__(self, key, val):
+        self.xx[key] = val.x
+        self.yy[key] = val.y
+
+
+cor_right = 1
+cor_none = 0
+cor_left = -1
+
+
+class occupied_corners(occupied):
+
+    def __init__(self, copy=None, cor_map=None, width=10, height=10):
+        super().__init__(copy, width, height)
+
+        if copy != None:
+            self.cor = copy.cor
+        else:
+            self.cor = np.full((width + 1, height + 1), cor_none, dtype=int)
+
+        if cor_map != None:
+            self.cor_map = cor_map
+        elif copy != None:
+            self.cor_map = copy.cor_map
+        else:
+            self.cor_map = index(*np.indices((width + 1, height + 1)))
+            self.cor_map[point(1, 2)] = point(5, 6)
+            # generate edge indices
+
+    def step(self, p, length, incoming):
+        super().step(p, length)
+
+    def eat(self, p, new_length, incoming):
+        super().eat(p, new_length)
